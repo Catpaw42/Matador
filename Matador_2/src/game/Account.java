d3e09be3908bb49d13740fb32bfa318c86d73f1c
Madsvj <Mads.viborg1@gmail.com>
2014-02-10 00:15:35 +0100

    Account og player klasserne oprettet og udfyldt.

--- Matador_2/src/game/Account.java
+++ Matador_2/src/game/Account.java
@@ -1,5 +1,47 @@
 package game;
 
 public class Account {
+	private int balance;
+	private int startbalance = 30000;
 
+	//-----------------------------------------------------------------
+	//  Laver en forøgelse af en konto.
+	//-----------------------------------------------------------------
+	public int deposit (int amount)
+	{
+		if (amount < 0)  // deposit value is negative
+		{
+			System.out.println("You cannot deposit negative amounts");
+		}
+		else
+			balance += amount;
+
+		return balance;
+	}
+
+	//-----------------------------------------------------------------
+	//  Trækker fra kontoen.
+	//-----------------------------------------------------------------
+	public int withdraw (int amount)
+	{
+		if (amount > balance)  // Sørger for den ikke trækker mere end den kan.
+		{
+			balance = 0;
+		}
+		else
+			balance -= amount;
+
+		return balance;
+	}
+
+	public int getBalance ()
+	{
+		return balance;
+	}	
+	public int setBalance (int k)
+	{
+		balance = k;
+
+		return balance;
+	}
 }
--- Matador_2/src/game/Player.java
+++ Matador_2/src/game/Player.java
@@ -1,6 +1,44 @@
 package game;
 
-public class Player
-{
-// der mangler indhold...
+public class Player {
+
+	private String name;
+	private String carColour;
+	private String carType;
+	private int position;
+	private Account accountOb = new Account();
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setFieldPlace(int position) {
+		this.position = position;
+	}
+	public Account getAccountOb() {
+		return accountOb;
+	}
+	public void setAccountOb(Account accountOb) {
+		this.accountOb = accountOb;
+	}
+	public String getcarColour() {
+		return carColour;
+	}
+	
+	public void setcarColour(String carColour) {
+		this.carColour = carColour;
+	}
+	
+	public String getcarType() {
+		return carType;
+	}
+	
+	public void setcarType(String carType) {
+		this.carColour = carType;
+	}
 }
