2d3fba8511458562d5b823712a9c696f61392ca0
Magnus B. S. <notcat42@noserver.fake>
2014-02-14 11:21:43 +0100

    Work on the initiation of the game

--- Matador_2/src/game/GameOptions.java
+++ Matador_2/src/game/GameOptions.java
@@ -2,5 +2,22 @@ package game;
 
 public class GameOptions
 {
+	//private int startingCapital;
+	private Player[] players;
+
+	public GameOptions()
+	{
+
+	}
+
+	public Player[] getPlayers()
+	{
+		return players;
+	}
+
+	public void setPlayers(Player[] players)
+	{
+		this.players = players;
+	}
 
 }
