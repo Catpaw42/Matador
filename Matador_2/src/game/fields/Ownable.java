87a66d3e0bb11aa50b14c3b4bbe771384236912f
Jakob <Jakob@10.16.152.55>
2014-02-21 08:49:45 +0100

    ownable

--- Matador_2/src/game/fields/Ownable.java
+++ Matador_2/src/game/fields/Ownable.java
@@ -1,11 +1,74 @@
 package game.fields;
 
+import entity.Player;
+import field.Ownable;
+
 public abstract class Ownable extends Field {
 
-	public Ownable(int nr, String name)
-	{
-		super(nr, name);
-		// TODO Auto-generated constructor stub
-	}
+	protected int price;
+	protected int[] rent;
+	protected Player owner = null;
+	protected Ownable[] serie;
+	protected int nr;
 
+	protected Ownable(String name, int price, int[] rent, int nr) {
+		super(name, true);
+		this.price = price;
+		this.rent = rent;
+	}
+	public abstract int getnr();
+	
+	public Ownable[] getSerie() {
+		return serie;
+	}
+	
+	public Player getOwner() {
+		return owner;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public boolean getStreet() {
+		return false;
+	}
+	
+	public void setOwner(Player ownerac) {
+		this.owner = ownerac;
+	}
+	
+	public char landOnField(Player player, int slag) {
+		
+		if(owner == null)
+			return 'u';
+		
+		else if(owner == player) {
+			return 'n';
+		}
+		else {
+			
+			int amount = rent(AntalGrunde(),slag);
+			if(player.changeBalance(amount,0))
+			{
+				owner.changeBalance(amount, 1);
+				return 'n';
+			}
+			
+			return 'f';
+		}
+	}
+	public abstract int rent(int number,  int multiplier);
+	
+	public int getRent(int multiplier) {
+		return rent(AntalGrunde() ,  multiplier);
+	}
+	
+	
+	
+	
+	
+	public String toString() {
+		return string();
+	}
 }
