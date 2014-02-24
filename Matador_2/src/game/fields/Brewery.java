58ff88f3ffd55c0313c1f3640d4f1fa57f99e0da
Jakob <Jakob@10.16.160.90>
2014-02-24 10:40:19 +0100

    brewery

--- Matador_2/src/game/fields/Brewery.java
+++ Matador_2/src/game/fields/Brewery.java
@@ -1,5 +1,21 @@
 package game.fields;
 
-public class Brewery {
+public class Brewery extends Ownable{
+
+	protected Brewery(String name, int price, int[] rent, int nr) {
+		super(name, price, rent, nr);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void message (){
+		if(getFieldNumber() == 12){
+			System.out.println("Du er landet på Tuborg");
+			
+		}
+		if(owner)
+	}
+	
+
+	
 
 }
