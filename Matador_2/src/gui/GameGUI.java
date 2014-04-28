3354ec83f9b416d6d6f58c3c58b343c2e44905c2
Madsvj <Mads.viborg1@gmail.com>
2014-04-28 21:50:20 +0200

    Mixed perfectionizing

--- Matador_2/src/gui/GameGUI.java
+++ Matador_2/src/gui/GameGUI.java
@@ -4,18 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.KeyboardFocusManager;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
 
 @SuppressWarnings("serial")
 public class GameGUI extends JFrame
